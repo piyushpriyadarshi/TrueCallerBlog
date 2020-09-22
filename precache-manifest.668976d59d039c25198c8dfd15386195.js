c3a6aa527c4ed5b0a09b54aebb91858b2e8c29f8
Piyush Priyadarshi <priyadarship85@gmail.com>
2020-09-22 12:14:56 +0530

    Updates

---
+++ precache-manifest.668976d59d039c25198c8dfd15386195.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7f47327332272f0779d8a1f15b825be",
+    "url": "/TrueCallerBlog/index.html"
+  },
+  {
+    "revision": "d785a793ffc8ba8eca6f",
+    "url": "/TrueCallerBlog/static/css/main.7ded77cf.chunk.css"
+  },
+  {
+    "revision": "a326b39dc42c2078ba15",
+    "url": "/TrueCallerBlog/static/js/2.3abd02ed.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TrueCallerBlog/static/js/2.3abd02ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d785a793ffc8ba8eca6f",
+    "url": "/TrueCallerBlog/static/js/main.b5c80a76.chunk.js"
+  },
+  {
+    "revision": "a2af54b423b9e92675ed",
+    "url": "/TrueCallerBlog/static/js/runtime-main.428f8ebd.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/TrueCallerBlog/static/media/logo.ee7cd8ed.svg"
+  }
+]);
